aa61813d879277b1eccc0bf864fa0027495b7a2c
Josh Mills <josh@trueheart78.com>
2021-06-21 15:36:28 -0400

    More specs for GemLookup::Flags

--- spec/gem_lookup/flags_spec.rb
+++ spec/gem_lookup/flags_spec.rb
@@ -2,14 +2,14 @@
 
 RSpec.describe GemLookup::Flags do
   describe '.supported' do
-    subject (:sorted_keys) { described_class.supported.keys.sort }
+    subject(:sorted_keys) { described_class.supported.keys.sort }
 
     it 'contains the expected keys' do
       expect(sorted_keys).to eq %i[help json version]
     end
 
     describe 'help' do
-      subject (:hash) { described_class.supported[:help] }
+      subject(:hash) { described_class.supported[:help] }
 
       let(:matches) { %w[-h --help] }
       let(:desc)    { 'Display the help screen.' }
@@ -24,7 +24,7 @@ RSpec.describe GemLookup::Flags do
     end
 
     describe 'json' do
-      subject (:hash) { described_class.supported[:json] }
+      subject(:hash) { described_class.supported[:json] }
 
       let(:matches) { %w[-j --json] }
       let(:desc)    { 'Display the raw JSON.' }
@@ -39,7 +39,7 @@ RSpec.describe GemLookup::Flags do
     end
 
     describe 'version' do
-      subject (:hash) { described_class.supported[:version] }
+      subject(:hash) { described_class.supported[:version] }
 
       let(:matches) { %w[-v --version] }
       let(:desc)    { 'Display version information.' }
@@ -55,6 +55,52 @@ RSpec.describe GemLookup::Flags do
   end
 
   describe '.supported?' do
+    subject(:supported) { described_class.supported? type, flags: flags }
 
+    let(:type)  { :help }
+    let(:flags) { [] }
+
+    context 'when the type is :help' do
+      let(:type)  { :help }
+
+      context 'with a short flag that is supported' do
+        let(:flags) { ['-h'] }
+
+        it { is_expected.to be true }
+      end
+
+      context 'with a flag that is supported' do
+        let(:flags) { ['--help'] }
+
+        it { is_expected.to be true }
+      end
+
+    end
+
+    context 'when passed a type that is not supported' do
+      context 'when the type is nil' do
+        let(:type) { nil }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when the type is unsupported' do
+        let(:type) { :unsupported }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when the type is an empty string' do
+        let(:type) { '' }
+
+        it { is_expected.to eq false }
+      end
+    end
+
+    context 'when passed an empty set of flags' do
+      let(:flags) { [] }
+
+      it { is_expected.to eq false }
+    end
   end
 end
