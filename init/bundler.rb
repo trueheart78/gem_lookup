18c38ebb5c319d12845a62c9c4a6063d842fe596
Josh Mills <josh@trueheart78.com>
2021-06-21 12:32:19 -0400

    New init dir for autoloading and detection of ENV["APP_ENV"]

---
+++ init/bundler.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default, ENV['APP_ENV'].to_sym)
---
+++ init/environment.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+ENV['APP_ENV'] = 'development' unless ENV['APP_ENV']
