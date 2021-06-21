8126a41e287637eecd1c8d4b022f46e969beb40c
Josh Mills <josh@trueheart78.com>
2021-06-21 14:49:48 -0400

    Moved gem_lookup_spec.rb to gem_lookup/version_spec.rb

--- spec/gem_lookup/version_spec.rb
+++ spec/gem_lookup/version_spec.rb
@@ -4,9 +4,4 @@ RSpec.describe GemLookup do
   it 'has a version number' do
     expect(GemLookup::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    useful = true
-    expect(useful).to eq(true)
-  end
 end
