3cb546ae384b33890bc8423b295125ed5e6e029d
Josh Mills <josh@trueheart78.com>
2021-06-21 15:50:38 -0400

    Added documentation

--- lib/gem_lookup/rate_limit.rb
+++ lib/gem_lookup/rate_limit.rb
@@ -2,6 +2,7 @@
 
 module GemLookup
   module RateLimit
+    # @return [Numeric] the max requests that can be made per second.
     MAX_REQUESTS_PER_SECOND = 10
   end
 end
--- lib/gem_lookup/version.rb
+++ lib/gem_lookup/version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 module GemLookup
+  # @return [String] the current version of the gem.
   VERSION = '0.8.0'
 end
