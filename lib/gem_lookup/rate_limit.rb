a3686df0d54fc36cc746d858f7506e90c30f6162
Josh Mills <josh@trueheart78.com>
2021-06-22 11:06:06 -0400

    Expanded RateLimit module

--- lib/gem_lookup/rate_limit.rb
+++ lib/gem_lookup/rate_limit.rb
@@ -7,5 +7,17 @@ module GemLookup
 
     # @return [String] the url that documents the rate limit
     RATE_LIMIT_DOCUMENTATION_URL = 'https://guides.rubygems.org/rubygems-org-rate-limits/'
+
+    # Calls MAX_REQUESTS_PER_SECOND.
+    # @return [Numeric] the max requests that can be made per second.
+    def self.number
+      MAX_REQUESTS_PER_SECOND
+    end
+
+    # Calls RATE_LIMIT_DOCUMENTATION_URL.
+    # @return [String] the url that documents the rate limit.
+    def self.documentation_url
+      RATE_LIMIT_DOCUMENTATION_URL
+    end
   end
 end
--- spec/gem_lookup/rate_limit_spec.rb
+++ spec/gem_lookup/rate_limit_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe GemLookup::RateLimit do
     url = 'https://guides.rubygems.org/rubygems-org-rate-limits/'
     expect(described_class::RATE_LIMIT_DOCUMENTATION_URL).to eq url
   end
+
+  describe '.number' do
+    subject(:number) { described_class.number }
+
+    it 'returns the rate limit' do
+      expect(number).to eq described_class::MAX_REQUESTS_PER_SECOND
+    end
+  end
+
+  describe '.documentation_url' do
+    subject(:documentation_url) { described_class.documentation_url }
+
+    it 'returns the documentation url' do
+      expect(documentation_url).to eq described_class::RATE_LIMIT_DOCUMENTATION_URL
+    end
+  end
 end
