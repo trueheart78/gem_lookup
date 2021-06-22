78fc19a7bd58fd6ebeb14b5c66c7cb769c52d77d
Josh Mills <josh@trueheart78.com>
2021-06-22 10:38:50 -0400

    Added NAME to GemLookup constants in version.rb

--- spec/gem_lookup/version_spec.rb
+++ spec/gem_lookup/version_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe GemLookup do
   it 'has a version number' do
     expect(described_class::VERSION).not_to be nil
   end
+
+  it 'has the expected name' do
+    expect(described_class::NAME).to eq 'gem_lookup'
+  end
 end
