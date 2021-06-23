1c3f147e2e841bc4a8ea9de188f5b08bc87f9c1f
Josh Mills <josh@trueheart78.com>
2021-06-23 16:41:02 -0400

    Minor RateLimit refactor

--- lib/gem_lookup/rate_limit.rb
+++ lib/gem_lookup/rate_limit.rb
@@ -11,22 +11,24 @@ module GemLookup
     # @return [String] the url that documents the rate limit
     RATE_LIMIT_DOCUMENTATION_URL = 'https://guides.rubygems.org/rubygems-org-rate-limits/'
 
-    # Calls MAX_REQUESTS_PER_SECOND.
-    # @return [Numeric] the max requests that can be made per interval.
-    def self.number
-      MAX_REQUESTS_PER_INTERVAL
-    end
+    class << self
+      # Calls MAX_REQUESTS_PER_SECOND.
+      # @return [Numeric] the max requests that can be made per interval.
+      def number
+        MAX_REQUESTS_PER_INTERVAL
+      end
 
-    # Calls FREQUENCY_INTERVAL_IN_SECONDS
-    # @return [Numeric] the frequency interval, in seconds.
-    def self.interval
-      FREQUENCY_INTERVAL_IN_SECONDS
-    end
+      # Calls FREQUENCY_INTERVAL_IN_SECONDS
+      # @return [Numeric] the frequency interval, in seconds.
+      def interval
+        FREQUENCY_INTERVAL_IN_SECONDS
+      end
 
-    # Calls RATE_LIMIT_DOCUMENTATION_URL.
-    # @return [String] the url that documents the rate limit.
-    def self.documentation_url
-      RATE_LIMIT_DOCUMENTATION_URL
+      # Calls RATE_LIMIT_DOCUMENTATION_URL.
+      # @return [String] the url that documents the rate limit.
+      def documentation_url
+        RATE_LIMIT_DOCUMENTATION_URL
+      end
     end
   end
 end
