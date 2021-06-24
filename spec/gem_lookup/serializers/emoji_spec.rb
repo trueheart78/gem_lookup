34507b681a2f80e21222b7cace90eeb097bd41e7
Josh Mills <josh@trueheart78.com>
2021-06-24 12:14:38 -0400

    Serializers::Emoji specs updated

--- spec/gem_lookup/serializers/emoji_spec.rb
+++ spec/gem_lookup/serializers/emoji_spec.rb
@@ -2,9 +2,39 @@
 
 RSpec.describe GemLookup::Serializers::Emoji do
   describe '.display'
-  describe '.gem_count'
-  describe '.batch_iterator'
-  describe '.querying'
+  describe '.gem_count' do
+    subject(:gem_count) do
+      capture_output { described_class.gem_count num }.chomp
+    end
+
+    let(:num)    { junk :int, min: 1, max: 1_000 }
+    let(:output) { "=> 🤔 #{num} gems".light_cyan }
+
+    it { is_expected.to eq output }
+  end
+
+  describe '.batch_iterator' do
+    subject(:batch_iterator) do
+      capture_output { described_class.batch_iterator num, total }.chomp
+    end
+
+    let(:num)    { junk :int, min: 1, max: 499 }
+    let(:total)  { junk :int, min: 500, max: 1_000 }
+    let(:output) { "=> 🧺 #{num} of #{total}".yellow }
+
+    it { is_expected.to eq output }
+  end
+
+  describe '.querying' do
+    subject(:querying) do
+      capture_output { described_class.querying batch }.chomp
+    end
+
+    let(:batch)  { [ junk, junk, junk, junk ] }
+    let(:output) { "=> 🔎 #{batch.join(', ')}".light_yellow }
+
+    it { is_expected.to eq output }
+  end
 
   describe '.streaming?' do
     it 'returns true' do
