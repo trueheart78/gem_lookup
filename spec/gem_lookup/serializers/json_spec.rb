0cda193f9a895006ec4f4aea136fefd420b79d3c
Josh Mills <josh@trueheart78.com>
2021-06-24 11:57:26 -0400

    More specs and comments for Serializers::Json

--- spec/gem_lookup/serializers/json_spec.rb
+++ spec/gem_lookup/serializers/json_spec.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe GemLookup::Serializers::Json do
+  describe '.display' do
+    let(:expected_output) { '' }
+
+    it 'prints out valid JSON' do
+      json = capture_output { described_class.display json: junk }
+
+      expect { JSON.parse json }.to_not raise_error
+    end
+  end
+
   describe '.streaming?' do
     it 'returns false' do
       expect(described_class).to_not be_streaming
