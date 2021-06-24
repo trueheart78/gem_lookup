f4b40d22ef0bf793fb4fc91ceaca88be7e152d1b
Josh Mills <josh@trueheart78.com>
2021-06-24 12:02:19 -0400

    Basic additions to the wordy and emoji specs

--- spec/gem_lookup/serializers/wordy_spec.rb
+++ spec/gem_lookup/serializers/wordy_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe GemLookup::Serializers::Wordy do
+  describe '.display'
+  describe '.gem_count'
+  describe '.batch_iterator'
+  describe '.querying'
+
   describe '.streaming?' do
     it 'returns true' do
       expect(described_class).to be_streaming
