c646db4760ed3603d7123b53c4ac5a10ab33c5ef
Josh Mills <josh@trueheart78.com>
2021-06-25 12:12:09 -0400

    New Errors: UnsupportedFlag and UnsupportedFlags

--- lib/gem_lookup/errors.rb
+++ lib/gem_lookup/errors.rb
@@ -3,5 +3,9 @@
 module GemLookup
   module Errors
     class InvalidDisplayMode < StandardError; end
+
+    class UnsupportedFlags < StandardError; end
+
+    class UnsupportedFlag < StandardError; end
   end
 end
--- spec/gem_lookup/errors_spec.rb
+++ spec/gem_lookup/errors_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe GemLookup::Errors do
       expect(definition).to eq 'constant'
     end
   end
+
+  describe 'UnsupportedFlag' do
+    subject(:definition) { defined? described_class::UnsupportedFlag }
+
+    it 'is defined as a constant' do
+      expect(definition).to eq 'constant'
+    end
+  end
+
+  describe 'UnsupportedFlags' do
+    subject(:definition) { defined? described_class::UnsupportedFlags }
+
+    it 'is defined as a constant' do
+      expect(definition).to eq 'constant'
+    end
+  end
 end
