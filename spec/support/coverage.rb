5bac3e2d41ab62256bf91ea6927a79be2c3aafaf
Josh Mills <josh@trueheart78.com>
2021-06-29 13:26:10 -0400

    SimpleCov minimum coverage increased to 90% from 80%.

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -5,7 +5,7 @@ module Support
     def self.included(_klass)
       require 'simplecov'
 
-      SimpleCov.minimum_coverage 80
+      SimpleCov.minimum_coverage 90
 
       SimpleCov.start do
         add_filter '/spec/'
