b9378bbb6b1f46ae05981409b6c836e9a6a10a81
Josh Mills <josh@trueheart78.com>
2021-07-14 11:36:15 -0400

    Update lib/gem_lookup.rb to fix two issues.
    * Satisfies Zeitwerk's need for GemLookup to be defined in the file.
    * Solves a GemLookup::Help.version constant check, as this module had
      not yet been defined.

--- lib/gem_lookup.rb
+++ lib/gem_lookup.rb
@@ -2,5 +2,6 @@
 
 require_relative '../booster_pack'
 
-module GemLookup
-end
+# Requiring satisfies Zeitwerk's need for GemLookup to be defined in this file.
+# It also solves a GemLookup::Help.version constant check, as this had yet to be loaded.
+require_relative 'gem_lookup/version'
